e1df3412792e81e2dca48b7a12c869abd106ac37
MariaLundgren <melundgren93@gmail.com>
2021-08-03 18:18:55 +0000

    Added products models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
