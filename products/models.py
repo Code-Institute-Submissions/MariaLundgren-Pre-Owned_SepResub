b2107ad8807f127a22232a92608a25dd9dab267b
MariaLundgren <melundgren93@gmail.com>
2021-08-04 11:14:23 +0000

    Added size field to product models

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     color = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
     image_url = models.URLField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
