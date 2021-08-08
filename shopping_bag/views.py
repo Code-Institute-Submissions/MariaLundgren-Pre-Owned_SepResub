bc116a2ae2835317a13ed7129470254bab3eb6d5
MariaLundgren <melundgren93@gmail.com>
2021-08-08 18:00:39 +0000

    started adding functionality to add products to shopping bag

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -5,3 +5,16 @@ from products.models import Product
 def shopping_bag(request):
 
     return render(request, 'shopping_bag/shopping_bag.html')
+
+
+def add_to_shopping_bag(request, product_id):
+
+    get_object_or_404(Product, pk=product_id)
+
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    shopping_bag[product_id]
+
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session["shopping_bag"])
+    return redirect('products')
