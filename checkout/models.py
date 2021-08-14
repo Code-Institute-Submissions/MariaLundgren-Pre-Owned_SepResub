7a71db7e4be37c9220217681a8cb20e47077e1a5
MariaLundgren <melundgren93@gmail.com>
2021-08-14 06:34:38 +0000

    created checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
+from django_countries.fields import CountryField
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=50, null=False, editable=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.CharField(max_length=50, null=True)
+    phone_number = models.CharField(max_length=25, null=True)
+    street_address = models.CharField(max_length=50, null=True)
+    postcode = models.CharField(max_length=25, null=True)
+    town_or_city = models.CharField(max_length=50, null=True)
+    country = CountryField(blank_label='Country *', null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        self.order_total = self.orderitems.aggregate(Sum('orderitem_total'))['orderitem_total_sum']
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='orderitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=False, blank=False)
+    product_price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.order.order_number}'
