40c1520e07278386f912596d52aadcd142100fe4
MariaLundgren <melundgren93@gmail.com>
2021-08-14 15:32:45 +0000

    added forms to checkout

---
+++ checkout/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Order
+
+
+class OredrForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('name', 'email', 'phone_number', 'street_address',
+                  'postcode', 'town_or_city', 'country',)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address': 'Street Address',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'country': 'Country',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
