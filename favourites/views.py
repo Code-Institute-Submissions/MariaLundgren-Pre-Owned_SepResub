52e0d1e6cb078e4242d2051a9638b6df00eaccd8
MariaLundgren <melundgren93@gmail.com>
2021-08-16 20:38:16 +0000

    added favourites views and content to tamplates

--- favourites/views.py
+++ favourites/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 
-def favourites(request):
+def favourites(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if product.favourites.filter(id=request.user.id).exists():
+        product.favourites.remove(request.user)
+    else:
+        product.favourites.add(request.user)
     return render(request, 'favourites/favourites.html')
+
+
+def favourite_list(request):
+    user = request.user
+
+    favourites = user.favourites.all()
+
+    context = {
+        'favourites': favourites,
+    }
+
+    return render(request, 'favourites/favourite_list.html', context)
--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
+app_name = "<products>"
+
 urlpatterns = [
     path('', views.products, name='products'),
     path('<product_id>', views.selected_product, name='selected_product'),
