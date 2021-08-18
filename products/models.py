e7d352c20a1edd699757b728807d0e8052917d4e
MariaLundgren <melundgren93@gmail.com>
2021-08-18 11:37:11 +0000

    changed name of favourites template and changed favourite field in models

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from profiles.models import UserProfile
+from profiles.models import User
 
 
 class Category(models.Model):
@@ -13,14 +13,15 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     color = models.CharField(max_length=100, null=True, blank=True)
     size = models.CharField(max_length=100, null=True, blank=True)
     image_url = models.URLField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    favourite = models.ManyToManyField(UserProfile, related_name='favourite', blank=True)
+    favourites = models.ManyToManyField(User, related_name='favourites', blank=True)
 
     def __str__(self):
         return self.name
