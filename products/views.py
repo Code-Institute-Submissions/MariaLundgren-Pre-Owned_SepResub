8ee07ab519b6054e7071cbaf5adb43b29dcc1bf0
MariaLundgren <melundgren93@gmail.com>
2021-08-26 19:05:02 +0000

    added functionality for links in navbar to sort clothes by category

--- products/views.py
+++ products/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 def products(request):
 
     products = Product.objects.all()
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            if sortkey == 'name':
+                products = products.annotate(name=('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
