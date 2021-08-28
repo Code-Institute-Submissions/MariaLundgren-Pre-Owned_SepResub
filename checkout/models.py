17506d570e0a85aa1d54fc47f7e9e4a052a2411c
MariaLundgren <melundgren93@gmail.com>
2021-08-28 14:48:34 +0000

    added user_profile to order

--- checkout/models.py
+++ checkout/models.py
@@ -6,10 +6,13 @@ from django.conf import settings
 
 from products.models import Product
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=50, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.CharField(max_length=50, null=True)
     phone_number = models.CharField(max_length=25, null=True)
