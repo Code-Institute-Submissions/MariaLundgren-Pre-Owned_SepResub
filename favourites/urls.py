c9e80fe8453c13eb8421163aba19cb661b61d1ec
MariaLundgren <melundgren93@gmail.com>
2021-09-01 10:31:45 +0000

    added remove function to favourites

--- favourites/urls.py
+++ favourites/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.favourites, name='favourites'),
     path('add_favourite/<product_id>', views.add_favourite, name='add_favourite'),
-    path('remove_favourite/<product_id>', views.remove_favourite, name='remove_favourite'),
+    path('remove/<product_id>', views.remove_favourite, name='remove_favourite'),
 ]
