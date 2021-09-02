e7d7ae4cd3821382ac6600f245db3dcc5e9101ff
MariaLundgren <melundgren93@gmail.com>
2021-09-02 05:28:11 +0000

    added login required and messages to contact views

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+@login_required
 def contact(request):
     if request.method == 'POST':
         form =ContactForm(request.POST)
         if form.is_valid():
+            messages.success(request, f'Thank you for your message!')
             contact = form.save(commit=False)
             contact.user = request.user
             contact.save()
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
-
