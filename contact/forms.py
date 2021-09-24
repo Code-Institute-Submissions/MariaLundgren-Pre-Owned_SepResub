96acd608ffed2e2ecd78c74c52a03cc60aca1954
MariaLundgren <melundgren93@gmail.com>
2021-09-24 18:30:14 +0000

    Add min_length validation to fields in contact form

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,8 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    subject = forms.CharField(min_length=5)
+    message = forms.CharField(min_length=20, widget=forms.Textarea)
     class Meta:
         model = Contact
         exclude = ('user', 'answer')
--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import RegexValidator
 
 class Contact(models.Model):
     user = models.ForeignKey(User, null=True, blank=True,
                              on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)
-    message = models.TextField()
+    message = models.TextField(max_length=500)
     answer = models.TextField(null=True, blank=True)
