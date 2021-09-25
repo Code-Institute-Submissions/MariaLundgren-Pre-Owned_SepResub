6ee67202a4a139bf19282a31a337e95eae1b53e9
MariaLundgren <melundgren93@gmail.com>
2021-09-25 11:55:32 +0000

    Change subject field on contact form to dropdown list

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from .models import Contact
+from django.db.models.fields import BLANK_CHOICE_DASH
+
+SUBJECT_CHOICES = [
+    ('returns', 'Returns'),
+    ('shipping', 'Shipping'),
+    ('product_question', 'Product question'),
+    ('other', 'Other')
+]
 
 
 class ContactForm(forms.ModelForm):
-    subject = forms.CharField(min_length=5)
+    subject = forms.CharField(widget=forms.Select(choices=BLANK_CHOICE_DASH + list(SUBJECT_CHOICES)))
     message = forms.CharField(min_length=20, widget=forms.Textarea)
     class Meta:
         model = Contact
