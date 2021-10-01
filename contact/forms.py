912c51162849c92304ccd1c30a4bd9b56e200ab3
MariaLundgren <melundgren93@gmail.com>
2021-10-01 21:12:56 +0000

    Add helper text to order field on contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Contact
 from django.db.models.fields import BLANK_CHOICE_DASH
+from django.utils.translation import ugettext_lazy as _
 
 SUBJECT_CHOICES = [
     ('returns', 'Returns'),
@@ -13,6 +14,12 @@ SUBJECT_CHOICES = [
 class ContactForm(forms.ModelForm):
     subject = forms.CharField(widget=forms.Select(choices=BLANK_CHOICE_DASH + list(SUBJECT_CHOICES)))
     message = forms.CharField(min_length=20, widget=forms.Textarea)
+
     class Meta:
         model = Contact
         exclude = ('user', 'answer')
+
+        help_texts = {
+            'order': _('If your message is concerning a specific order, please choose\
+                the order number to make it easier and faster for us to awnser you.'),
+        }
